690c371726f507207a313ec3d896a4db804ad0bf
HuuTri130401 <trith130401@gmail.com>
2025-01-04 23:51:33 +0700

    Update: get list user

---
+++ Application/Models/BaseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class BaseModel
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset? Created { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public DateTimeOffset? Updated { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        public bool? Deleted { get; set; } = false;
+        public bool? IsActive { get; set; } = true;
+        public string? CreatedByName { get; set; }
+        public string? CreatedByCode { get; set; }
+    }
+}
